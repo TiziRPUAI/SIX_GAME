cdbdabb2719edd30b80c6c33703016ef1a9d8753
TiziRPUAI <Tiziano.Mancini@alumnos.uai.edu.ar>
2025-10-29 19:07:32 -0300

    Cambios

---
+++ Assets/scripts/InicializadorEscena.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class InicializadorEscena : MonoBehaviour
+{
+    void Awake()
+    {
+        // Asegurarse de que el tiempo est茅 en escala normal
+        Time.timeScale = 1f;
+
+        Debug.Log("Escena inicializada - Time.timeScale restaurado a 1");
+    }
+}
